2ddb7914c67d09bac1127b3a77bd24ec4db7be20
countstarlight <countstarlight@outlook.com>
2016-03-22 22:37:46 -0400

    Some changes for OPPO R7 plusm.

--- mokee.mk
+++ mokee.mk
@@ -1,15 +1,37 @@
-# Release name
-PRODUCT_RELEASE_NAME := r7plusm
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit some common CM stuff.
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, device/oppo/r7plusm/device.mk)
+
+# Inherit some common MK stuff.
 $(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/oppo/r7plusm/full_r7plusm.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := r7plusm
 PRODUCT_NAME := mk_r7plusm
+PRODUCT_DEVICE := r7plusm
 PRODUCT_BRAND := OPPO
-PRODUCT_MODEL := r7plusm
+PRODUCT_MODEL := R7plusm
 PRODUCT_MANUFACTURER := OPPO
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=OPPO/R7plusm/R7Plusm:5.1.1/LMY47V/1437558400:user/release-keys \
+    PRIVATE_BUILD_DESC="mt6795_64-user 5.1.1 LMY47V eng..20150807.023105 dev-keys" \
+    TARGET_DEVICE="R7plusm"
