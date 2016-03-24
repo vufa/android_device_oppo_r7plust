5fdfe85da8f05c13da2ab8a767732ec3d7df8a30
countstarlight <countstarlight@outlook.com>
2016-03-23 23:43:49 -0400

    Make some changes for oppo r7 plusm.

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
-$(call inherit-product, device/oppo/r7plusm/vendor/copyfiles.mk)
+#$(call inherit-product, device/oppo/r7plusm/vendor/copyfiles.mk)
 $(call inherit-product, vendor/oppo/r7plusm/r7plusm-vendor-blobs.mk)
 
 LOCAL_PATH := device/oppo/r7plusm
