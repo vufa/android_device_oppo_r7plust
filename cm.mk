ae5c5f1f31b30793660036f3e80e806eb98b762c
countstarlight <countstarlight@outlook.com>
2016-04-01 21:19:16 -0400

    Fix some files for OPPO R7 plus(China moblie).

---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := r7plusm
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/oppo/r7plusm/full_r7plusm.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := r7plusm
+PRODUCT_NAME := cm_r7plusm
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := r7plusm
+PRODUCT_MANUFACTURER := OPPO
