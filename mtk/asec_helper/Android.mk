a75d2e7f88ee3aea1794f9b02e45412f9e3b4d65
countstarlight <countstarlight@outlook.com>
2016-05-09 23:51:19 +0800

    FIx some files.

--- mtk/asec_helper/Android.mk
+++ mtk/asec_helper/Android.mk
@@ -6,5 +6,6 @@ LOCAL_MODULE_TAGS             := optional
 LOCAL_SRC_FILES               := asec_helper.c
 LOCAL_SHARED_LIBRARIES        := \
 	libcutils \
-	liblog
+	liblog \
+        libc
 include $(BUILD_EXECUTABLE)
